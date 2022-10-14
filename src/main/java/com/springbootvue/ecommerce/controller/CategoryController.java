f67964b38d529b1bb72b1044ca0b94438273b5ad
timstats91 <timstats91@gmail.com>
2022-10-14 14:05:23 -0500

    Adding CORS annotation and configuring MySQL database

--- src/main/java/com/springbootvue/ecommerce/controller/CategoryController.java
+++ src/main/java/com/springbootvue/ecommerce/controller/CategoryController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,17 +24,20 @@ public class CategoryController {
 	@Autowired
 	CategoryService categoryService;
 
+	@CrossOrigin(origins = "http://localhost:8081")
 	@PostMapping("/create")
 	public ResponseEntity<ApiResponse> createCategory(@RequestBody Category category) {
 		categoryService.createCategory(category);
 		return new ResponseEntity<>(new ApiResponse(true, "new category was created"), HttpStatus.CREATED);
 	}
 	
+	@CrossOrigin(origins = "http://localhost:8081")
 	@GetMapping("/list")
 	public List<Category> listCategory() {
 		return categoryService.listCategory();
 	}
 	
+	@CrossOrigin(origins = "http://localhost:8081")
 	@PostMapping("/update/{categoryId}")
 	public ResponseEntity<ApiResponse> updateCategory(@PathVariable("categoryId") int categoryId, @RequestBody Category category) {
 		System.out.println("category id " + categoryId);
